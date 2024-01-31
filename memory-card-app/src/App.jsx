700fd6c6e21c7ae04ae47a852d06c2467c5ef8dc
Abhishek Govindaraj <abhishekgh3103@gmail.com>
2024-01-31 08:43:07 +0530

    Add Header component content and style

--- memory-card-app/src/App.jsx
+++ memory-card-app/src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
   return (
     <>
-      <Header />
+      <Header currentScore={currentScore} bestScore={bestScore} />
     </>
   );
 }
--- memory-card-app/src/components/Header.jsx
+++ memory-card-app/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import styles from "./header.module.css";
 
-export default function Header() {
+export default function Header({ currentScore, bestScore }) {
   return (
-    <div>
-      <h1>Memory Card Game</h1>
+    <div className={styles.headerDiv}>
+      <div className={styles.h1Div}>
+        <h1>Memory Card Game</h1>
+      </div>
+      <div className={styles.scoreDiv}>
+        <p>Current Score: {currentScore}</p>
+        <p>Best Score: {bestScore}</p>
+      </div>
     </div>
   );
 }
